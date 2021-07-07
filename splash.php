7f8213bd9aad19530d9b79ea1797cef24e8c446b
code-maestro <michaelajnew@gmail.com>
2021-07-07 17:53:59 +0300

    Refactor the splash html file to php ✔

--- splash.php
+++ splash.php
@@ -1,3 +1,39 @@
+
+<?php
+
+include 'database/db_module.php';
+include 'modules/signup.php';
+
+$regno = "";
+$email = "";
+$fname = "";
+$lname = "";
+$password = "";
+$confirmedPassword = "";
+
+if (isset($_POST['register'])) {
+
+    $signup = new Signup();
+    $result = $signup->evaluate($_POST);
+
+    if ($result != "") {
+        echo $result;
+    }else {
+        header("Location: login.php");
+        die;
+    }
+
+    $regno = $_POST['regno'];
+    $email = $_POST['email'];
+    $fname = $_POST['first_name'];
+    $lname = $_POST['last_name'];
+    $password = $_POST['password'];
+    $confirmedPassword = $_POST['confirm_password'];
+    
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,10 +59,10 @@
         <p><a href="#">Sign up </a></p>
       </div>
       <div class="signup">
-        <input type="text" name="regno" id="regno" placeholder="Enter Registration Number">
-        <input type="email" name="email" id="email" placeholder="Enter email">
-        <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname ">
-        <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname">
+        <input type="text" name="regno" id="regno" placeholder="Enter Registration Number" value="">
+        <input type="email" name="email" id="email" placeholder="Enter email" value="">
+        <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname " value="">
+        <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname" value="">
         <input type="password" name="password" id="password" placeholder="Enter Password">
         <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
         <input type="submit" name="register" value="Sign up ">
