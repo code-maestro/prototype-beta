6f7d4a0dbeb8bd2ba5a1373dc4f71601efefcd56
code-maestro <michaelajnew@gmail.com>
2021-07-12 13:42:29 +0300

    Fix for the merge conflicts splash.php

--- splash.php
+++ splash.php
@@ -157,39 +157,41 @@
               <a href="#" class="student" id="student">STUDENT</a>
               <a href="#" class="staff" id="staff">STAFF</a>
             </div>
-              <div class="overlay-signup">
-                <div class="student-signup" id="student-signup">                
-                  <form onsubmit="return verifyPassword()" action="" method="post" >
-                    <input type="number" name="acc_type" id="acc_type" hidden value="1">
-                    <input type="text" name="regno" id="regno" placeholder="Enter Registration Number" required value="<?php echo $regno; ?>">
-                    <input type="email" name="email" id="email" placeholder="Enter email" required value="<?php echo $email; ?>">
-                    <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname" required value="<?php echo $fname; ?>">
-                    <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname" required value="<?php echo $lname; ?>">
-                    <input type="password" name="password" id="password" placeholder="Enter Password" required>
-                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
-                    <!-- SUBMIT / SIGN UP BUTTON -->
-                    <input type="submit" name="std_register" value="Sign up ">
-                  </form>
-                </div>
-
-                <div class="staff-signup " id="staff-signup">
-                  <form onsubmit="return verifyStaffPassword()" action="" method="post">
-                    <input type="number" name="acc_type" id="acc_type" hidden value="2">
-                    <input type="text" name="regno" id="regno" hidden value="  ">
-                    <input type="email" name="email" id="email" placeholder="Enter email" required>
-                    <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname" required>
-                    <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname" required>
-                    <input type="password" name="password" id="password" placeholder="Enter Password" required>
-                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
-                    <!-- SUBMIT / SIGN UP BUTTON -->
-                    <input type="submit" name="staff_register" value="Sign up ">
-                  </form>
-
-                </div>
+            <div class="overlay-signup">
+              <!-- STUDENT SIGNUP CONTAINER -->
+              <div class="student-signup" id="student-signup">                
+                <form onsubmit="return verifyPassword()" action="" method="post" >
+                  <input type="number" name="acc_type" id="acc_type" hidden value="1">
+                  <input type="text" name="regno" id="regno" placeholder="Enter Registration Number" required value="<?php echo $regno; ?>">
+                  <input type="email" name="email" id="email" placeholder="Enter email" required value="<?php echo $email; ?>">
+                  <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname" required value="<?php echo $fname; ?>">
+                  <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname" required value="<?php echo $lname; ?>">
+                  <input type="password" name="password" id="password" placeholder="Enter Password" required>
+                  <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
+                  <!-- SUBMIT / SIGN UP BUTTON -->
+                  <input type="submit" name="std_register" value="Sign up ">
+                </form>
               </div>
 
+              <!-- STAFF SIGNUP CONTAINER -->
+              <div class="staff-signup " id="staff-signup">
+                <form onsubmit="return verifyStaffPassword()" action="" method="post">
+                  <input type="number" name="acc_type" id="acc_type" hidden value="2">
+                  <input type="text" name="regno" id="regno" hidden value="  ">
+                  <input type="email" name="email" id="email" placeholder="Enter email" required>
+                  <input type="text" name="first_name" id="first_name" placeholder="Enter Firstname" required>
+                  <input type="text" name="last_name" id="last_name" placeholder="Enter Lastname" required>
+                  <input type="password" name="password" id="password" placeholder="Enter Password" required>
+                  <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
+                  <!-- SUBMIT / SIGN UP BUTTON -->
+                  <input type="submit" name="staff_register" value="Sign up ">
+                </form>
+              </div>
+            </div>
           </div>
         </div>
+
+        <!-- LOGIN CONTAINER -->
         <div class="form-container sign-in-container">
           <form method="POST" class="form">
             <h1> Log in</h1>
@@ -202,6 +204,7 @@
             <input id="login" type="submit" name="login" value="LOG IN">
           </form>
         </div>
+
         <div class="overlay-container">
           <div class="overlay">
             <div class="overlay-panel overlay-left">
