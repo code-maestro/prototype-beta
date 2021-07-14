ecff9350d7628344b2e9544e07f3f71ccec0ae56
code-maestro <michaelajnew@gmail.com>
2021-07-14 09:43:30 +0300

    β - Fix for logout new conditions

--- logout.php
+++ logout.php
@@ -3,13 +3,24 @@
   session_start();
 
   // Checking if session variable has data
-  if (isset($_SESSION['id'])) {
+  if (isset($_SESSION['staff_id'])) {
   
     // Setting the session variable to null
-    $_SESSION['id'] = NULL;
+    $_SESSION['staff_id'] = NULL;
 
     // Deleting the userdata from the session variable
-    unset($_SESSION['id']);
+    unset($_SESSION['staff_id']);
+
+  }
+
+  // Checking if session variable has data
+  if (isset($_SESSION['std_id'])) {
+  
+    // Setting the session variable to null
+    $_SESSION['std_id'] = NULL;
+
+    // Deleting the userdata from the session variable
+    unset($_SESSION['std_id']);
 
   }
 
