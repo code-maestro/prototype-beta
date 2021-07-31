c589d3ebd98bc8a4c41876610c2f79e1537915cb
code-maestro <michaelajnew@gmail.com>
2021-07-31 14:50:21 +0300

    β - Retrieveing the messages for the notification pane

---
+++ modules/notifications.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+
+    if(isset($_SESSION['std_id'])){
+
+      include 'user.php';
+      include '../database/db_module.php';
+
+      $out_id = $_SESSION['std_id'];
+
+      print_r($out_id);
+
+      $users = new User();
+
+      $the_id = $users->getChatID($out_id);
+
+      print_r($the_id);
+
+      //$in_id = "";
+      $in_id = $the_id['received_msg_id'];
+
+      $output = $users->getChatData($in_id, $out_id);
+        
+      $result = "";
+
+      if ($output) {                 
+        foreach ($output as $ROW) {
+
+          if($ROW['received_msg_id'] === $out_id){
+
+            $result .= ' <li>
+                            <span class="notification-title"> 23 </span>
+                            <i class="fas fa-circle"></i>
+                          </li>';
+
+          }
+        }
+      }  else{
+
+        $result .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
+      
+      }
+      
+      echo $result;
+
+    }else{
+
+      header("location: ./index.php");
+
+    }
+
+?>
