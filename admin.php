26f29e1f127a03e047c4d101d9ce4f6e6f8eca32
code-maestro <michaelajnew@gmail.com>
2021-08-12 15:46:26 +0300

    β - Implements the sorting of appointment by desc

--- admin.php
+++ admin.php
@@ -22,7 +22,7 @@
   $sql = "SELECT appointment_id, complaint, appointment_date, start_time, end_time, users_id, first_name, last_name, reg_no, email 
           FROM appointments INNER JOIN users INNER JOIN login 
           WHERE users.users_id = appointments.users_uid 
-          AND appointments.users_uid = login.users_uid;";
+          AND appointments.users_uid = login.users_uid ORDER BY appointments.id DESC";
 
   $sql_pending = "SELECT appointment_id, complaint, appointment_date, start_time, end_time, users_id, first_name, last_name, reg_no, email 
                    FROM appointments INNER JOIN users INNER JOIN login
