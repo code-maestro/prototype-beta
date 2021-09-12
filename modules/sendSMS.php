905e867f665978891f4b3d8d29dbf079e936d1ca
code-maestro <michaelajnew@gmail.com>
2021-09-12 11:40:16 +0300

    β - Refactoring the twilio api setup 🤗

--- modules/sendSMS.php
+++ modules/sendSMS.php
@@ -1,13 +1,18 @@
 <?php
-// Get the PHP helper library from https://twilio.com/docs/libraries/php
-// require_once '/path/to/vendor/autoload.php';
 
-// use Twilio\Rest\Client;
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once '../vendor/autoload.php';
 
-// Your Account Sid and Auth Token from twilio.com/user/account
-$sid = getenv('TWILIO_ACCOUNT_SID');
-$token = getenv('TWILIO_AUTH_TOKEN');
+use Twilio\Rest\Client;
 
-echo $sid . "  kjflsdkjfljf";
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+$sid = 'AC27f318bd622757b060a45f10658f90d0'; 
+$token ='1025ca67b99cd1bf16d8128ee7f01d31'; 
 
-?>
+$twilio = new Client($sid, $token);
+
+$message = $twilio->messages->create("+256703781554", ["body" => "Hi there", "from" => "+13236854720"]);
+
+print($message->sid);
